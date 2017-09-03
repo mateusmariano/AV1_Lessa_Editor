ff9a202791939afc707c5185397ebaf8226033e5
Mateus Mariano(buzz) <mtdmtdmtd3@gmail.com>
2017-09-03 17:35:12 -0300

    Add files via upload
    Now with tag applicator

--- AV1_Lessa_Editor_List/Assets/Destroy.cs
+++ AV1_Lessa_Editor_List/Assets/Destroy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#region assemblies
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
@@ -8,23 +9,36 @@ using UnityEditor.AnimatedValues;
 using UnityEngine.UI;
 using System.Reflection;
 using Type = System.Type;
+using UnityEditorInternal;
+#endregion
+public class BuzzDrops : EditorWindow {
 
 
-public class AdicionarElementos : EditorWindow {
-	
+	#region var
 	AnimBool m_ShowExtraFields;
 	AnimBool m_ShowExtraFields2;
 	public string[] options = new string[] {"rigidbody", "sphere collider", "mesh collider","light"};
 	public string[] options2 = new string[] {"cube", "sphere", "plane"};
+	public string[] options3 = InternalEditorUtility.tags;
 	public static int index = 0;
 	public static int index2 = 0;
+	public static int index3 = 0;
+	string hour;
 	bool change; 
-
-	[MenuItem("Buzz/Drops #k")]
+	public string Folder;
+	int index_screenshot;
+	#endregion
+		
+	
+	#region location of the Editor drop
+	[MenuItem("Buzz/Drops #Q")]
 	static void Init(){
-		EditorWindow window = GetWindow(typeof(AdicionarElementos));
-	}
+		EditorWindow window = GetWindow(typeof(BuzzDrops));
 
+	}
+	#endregion
+
+	#region Enable
 	void OnEnable()
 	{
 		m_ShowExtraFields = new AnimBool(false);
@@ -32,10 +46,13 @@ public class AdicionarElementos : EditorWindow {
 		m_ShowExtraFields2 = new AnimBool(false);
 		m_ShowExtraFields2.valueChanged.AddListener(Repaint);
 	}
+	#endregion
+	#region GUI and buttons
+
 
 	void OnGUI(){
 		//GUILayout.BeginHorizontal();
-		
+
 		GUIStyle gs = new GUIStyle();
 		/*Color cl =  EditorGUILayout.ColorField();
 		GUI.color = cl;*/
@@ -45,31 +62,45 @@ public class AdicionarElementos : EditorWindow {
 		if (EditorGUILayout.BeginFadeGroup(m_ShowExtraFields.faded))
 		{
 		index = EditorGUILayout.Popup(index, options);
-
-		if (GUILayout.Button("Colocar componente"))
+		if (GUILayout.Button("Colocar componente",GUILayout.Width(150),GUILayout.Height(20)))
 			Execute();
-		index2 = EditorGUILayout.Popup(index2, options2);
-			
-		if (GUILayout.Button("Criar objeto"))
-			InstantiatePrimitive();
-		if (GUILayout.Button("Destruir objeto"))
-			Destruir();
 			GUILayout.Space(10);
-			EditorGUILayout.HelpBox("Buzz drops @Copyright 2017. All rights reserved.", MessageType.Error);
-		
+
+		index2 = EditorGUILayout.Popup(index2, options2);
+		if (GUILayout.Button("Criar objeto",GUILayout.Width(150),GUILayout.Height(20)))
+				InstantiatePrimitive();
+			GUILayout.Space(10);
+
+		index3 = EditorGUILayout.Popup(index3,options3);
+		if (GUILayout.Button("Colocar tag",GUILayout.Width(150),GUILayout.Height(20)))
+				TheTag();
+			GUILayout.Space(10);
+
+
+		foreach(GameObject obj in Selection.gameObjects){
+			string name = obj.name;
+			EditorGUILayout.TextArea("Objetos selecionados");
+			EditorGUILayout.TextField(name);
+			}
+		if (GUILayout.Button("Destruir objetos",GUILayout.Width(150),GUILayout.Height(20)))
+			Destruir();
+			GUILayout.Space(20);
 		
 	}
 		EditorGUILayout.EndFadeGroup();
-		GUILayout.Label("Relogio: " + System.DateTime.Now.ToString());
-		GUILayout.Label("Computador do : " + System.Environment.UserName);
 
+		GUILayout.Label("Relogio " + System.DateTime.Now.ToString());	
+		GUILayout.Label("Computador do : " + System.Environment.UserName);
+		EditorGUILayout.HelpBox("Buzz drops @Copyright 2017. All rights reserved.", MessageType.Error);
 		if(EditorGUILayout.BeginFadeGroup(m_ShowExtraFields2.faded)){
 			EditorGUILayout.LabelField("Programei e sai correndo, pau no cu de quem ta lendo");
 		}
 		EditorGUILayout.EndFadeGroup();
-	//GUILayout.EndHorizontal();
 
 	}
+	#endregion
+
+	#region Voids
 
 	static void Execute(){
 		foreach (GameObject obj in Selection.gameObjects) {
@@ -117,5 +148,33 @@ public class AdicionarElementos : EditorWindow {
 		}
 
 	}
+	void TheTag(){
+		foreach(GameObject obj in Selection.gameObjects){
+			switch(index3){
+			case 0 : 
+				obj.tag = InternalEditorUtility.tags[index3];
+				break;
+			case 1 : 
+				obj.tag = InternalEditorUtility.tags[index3];
+				break;
+			case 2 : 
+				obj.tag = InternalEditorUtility.tags[index3];
+				break;
+			case 3 : 
+				obj.tag = InternalEditorUtility.tags[index3];
+				break;
+			case 4 : 
+				obj.tag = InternalEditorUtility.tags[index3];
+				break;
+			case 5 : 
+				obj.tag = InternalEditorUtility.tags[index3];
+				break;
+			case 6 : 
+				obj.tag = InternalEditorUtility.tags[index3];
+				break;
+			}
+		}
+	}
+	#endregion
 
 }
